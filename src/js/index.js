c6e5ee137f102d883bcfdba1665b533acb47d381
murtazaBulut <bulut.2097@gmail.com>
2022-10-15 11:50:03 +0300

    fix swiper bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
-import "./swiper.js"
+import "./swiper/lazyloading.js"
+import "./swiper/slider.js"
 
 console.log("Test index.js")
--- src/js/swiper/slider.js
+++ src/js/swiper/slider.js
@@ -1,13 +1,12 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
-var swiper = new Swiper(".swiper", {
+var slider = new Swiper(".slider", {
   slidesPerView: 2,
   spaceBetween: 5,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
   breakpoints: {
     640: {
       slidesPerView: 3,
